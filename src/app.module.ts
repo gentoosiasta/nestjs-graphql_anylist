203bbe631dfb592d3d03db9bb4f0dfcaab209c2f
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2023-01-16 11:16:54 -0600

    GraphQL support added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+    }),
+    ItemsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
