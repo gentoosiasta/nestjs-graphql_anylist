e16b2c1a64ec6d5ca380acef07b7b9436282d9a5
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2023-01-21 23:26:00 -0600

    :sparkles: Adds Items endpoints

--- src/items/dto/inputs/create-item.input.ts
+++ src/items/dto/inputs/create-item.input.ts
@@ -1,11 +1,19 @@
 import { InputType, Int, Field, Float } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, IsPositive, IsString } from 'class-validator';
 
 @InputType()
 export class CreateItemInput {
+  @IsNotEmpty()
+  @IsString()
   @Field(() => String)
   name: string;
+
+  @IsPositive()
   @Field(() => Float)
   quantity: number;
+
+  @IsString()
+  @IsOptional()
   @Field(() => String, { nullable: true })
-  quantityUnits: string;
+  quantityUnits?: string;
 }
