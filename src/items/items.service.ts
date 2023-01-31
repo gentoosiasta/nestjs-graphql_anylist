40a350dca469554aa253512b0fedc2ffa02d3db6
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2023-01-31 16:09:30 -0600

    :art: Complements Item-User tables relationship queries

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
 import { Item } from './entities/item.entity';
@@ -11,26 +12,36 @@ export class ItemsService {
     private readonly itemsRepository: Repository<Item>,
   ) {}
 
-  async create(createItemInput: CreateItemInput): Promise<Item> {
-    const newItem = this.itemsRepository.create(createItemInput);
+  async create(createItemInput: CreateItemInput, user: User): Promise<Item> {
+    const newItem = this.itemsRepository.create({ ...createItemInput, user });
 
     return await this.itemsRepository.save(newItem);
   }
 
-  async findAll(): Promise<Item[]> {
+  async findAll(user: User): Promise<Item[]> {
     // TODO: filtrar, paginar, por usuario...
-    return this.itemsRepository.find();
+    return this.itemsRepository.find({ where: { user: { id: user.id } } });
   }
 
-  async findOne(id: string): Promise<Item> {
-    const item = await this.itemsRepository.findOneBy({ id });
+  async findOne(id: string, user: User): Promise<Item> {
+    const item = await this.itemsRepository.findOneBy({
+      id,
+      user: { id: user.id },
+    });
 
     if (!item) throw new NotFoundException(`Item with id: ${id} not found`);
+
+    // item.user = user;
 
     return item;
   }
 
-  async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
+  async update(
+    id: string,
+    updateItemInput: UpdateItemInput,
+    user: User,
+  ): Promise<Item> {
+    await this.findOne(id, user);
     const item = await this.itemsRepository.preload(updateItemInput);
 
     if (!item) throw new NotFoundException(`Item with id: ${id} not found`);
@@ -38,9 +49,9 @@ export class ItemsService {
     return this.itemsRepository.save(item);
   }
 
-  async remove(id: string): Promise<Item> {
+  async remove(id: string, user: User): Promise<Item> {
     //TODO: soft delete, referential integrity
-    const item = await this.findOne(id);
+    const item = await this.findOne(id, user);
     await this.itemsRepository.remove(item);
 
     return { ...item, id };
