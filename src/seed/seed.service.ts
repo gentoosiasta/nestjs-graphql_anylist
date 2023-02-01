ace32a6d7c1878ea1837969af3260be96caab413
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2023-02-01 12:03:43 -0600

    :sparkles: Adds deleteDatabase functionality to Seed mutation

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from 'src/items/entities/item.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {
     this.isProd = configService.get('STATE') === 'prod';
   }
 
@@ -14,7 +26,16 @@ export class SeedService {
       throw new UnauthorizedException('We cannot run SEED on Production');
     }
 
-    
+    await this.deleteDatabase();
+
     return true;
+  }
+
+  async deleteDatabase() {
+    await this.itemsRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
   }
 }
